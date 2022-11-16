71a5f1be2f1175d7932c6a3d00129be5680f4acf
gagedezoort <jdezoort@gmail.com>
2022-11-15 21:48:12 -0600

    update mass points in job submission, default memory allocation

--- src/condor/submit.py
+++ src/condor/submit.py
@@ -29,7 +29,7 @@ def parse_args():
     add_arg("--script", default="run_analysis.py")
     add_arg("-v", "--verbose", action="store_true")
     add_arg("--show-config", action="store_true")
-    add_arg("--files-per-job", default=10)
+    add_arg("--files-per-job", default=1)
     add_arg("--mass", default="all")
     return parser.parse_args()
 
